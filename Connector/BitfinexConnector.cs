bb948175fe50e5064d788c0e87431574b586f299
Vazovsk1y <anolrey.b@gmail.com>
2025-02-17 18:56:08 +0300

    -initial commit.

---
+++ Connector/BitfinexConnector.cs
@@ -0,0 +1,40 @@
+﻿using Connector.Contracts;
+
+namespace Connector;
+
+internal class BitfinexConnector : ITestConnector
+{
+    public event Action<Trade>? NewBuyTrade;
+    public event Action<Trade>? NewSellTrade;
+    public event Action<Candle>? CandleSeriesProcessing;
+    
+    public Task<IEnumerable<Trade>> GetNewTradesAsync(string pair, int maxCount)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Candle>> GetCandleSeriesAsync(string pair, int periodInSec, DateTimeOffset? from, DateTimeOffset? to = null, long? count = 0)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public void SubscribeTrades(string pair, int maxCount = 100)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UnsubscribeTrades(string pair)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SubscribeCandles(string pair, int periodInSec, DateTimeOffset? from = null, DateTimeOffset? to = null, long? count = 0)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UnsubscribeCandles(string pair)
+    {
+        throw new NotImplementedException();
+    }
+}
